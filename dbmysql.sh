2049be3a04556502a6a6023c31d2bef48111a03c
nirmalaMahadasyam <nirmalaMahadasyam@github.com>
2024-05-01 15:22:51 +0530

    newcode

--- dbmysql.sh
+++ dbmysql.sh
@@ -8,8 +8,8 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
-# echo "Please enter DB password:"
-# read -s mysql_root_password
+ echo "Please enter DB password:"
+ read -s mysql_root_password
 
 VALIDATE(){
    if [ $1 -ne 0 ]
@@ -42,12 +42,12 @@ VALIDATE $? "Starting MySQL Server"
 # mysql_secure_installation --set-root-pass ExpenceApp@1 &>>$LOGFILE
 # VALIDATE $? "Setting up root password"
 
- mysql -h dbsql.nirmaladevops.cloud -uroot -pExpenceApp@1  -e 'show databases;'
+ mysql -h dbsql.nirmaladevops.cloud -uroot -p ${mysql_root_password}  -e 'show databases;'
 
 if [ $? -ne 0 ]
 then
     # mysql_secure_installation --set-root-pass ${mysql_root_password} &>>$LOGFILE
-    mysql_secure_installation --set-root-pass ExpenceApp@1 &>>$LOGFILE
+    mysql_secure_installation --set-root-pass ${mysql_root_password} &>>$LOGFILE
     VALIDATE $? "MySQL Root password Setup"
 else
     echo -e "MySQL Root password is already setup...$Y SKIPPING $N"
